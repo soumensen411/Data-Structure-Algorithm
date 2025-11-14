acab7772a41475fc127a55df14dd03e63b3db23f
SoumenSen31 <soumensen411@gmail.com>
2025-11-14 22:20:22 +0600

    add delete function in heap

--- Tree/Heap/heap.cpp
+++ Tree/Heap/heap.cpp
@@ -1,14 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Heap{
+class Heap
+{
 public:
     vector<int> v;
     int size;
 
     Heap()
     {
-        size = 0;   
+        size = 0;
     }
+
     void insertAtHeap(int value)
     {
         size++;
@@ -16,24 +18,51 @@ public:
         int index = v.size() - 1;
         while (index > 0)
         {
-            int parentIndex = (index - 1)/2;
+            int parentIndex = (index - 1) / 2;
+
             if (v[index] > v[parentIndex])
             {
                 swap(v[index], v[(index - 1) / 2]);
                 index = parentIndex;
             }
             else
-            {
                 break;
-            }
         }
     }
+
+    void deleteHeapElement()
+    {
+        if (size == 0)
+        {
+            cout << "heap is already empty\n";
+            return;
+        }
+        v[0] = v[size - 1];
+        size--;
+        v.pop_back();
+        int index = 0;
+        while (true)
+        {
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
+            int largest = index;
+            if (left > size && v[left]>v[largest]){
+                largest = left;
+            }
+            if(right > size && v[right]>v[largest]){
+                largest = right;
+            }
+            else break;
+        }
+    }
+
     void printHeadElement()
     {
         for (int i = 0; i < size; i++)
         {
             cout << v[i] << " ";
         }
+        cout << endl;
     }
 };
 
@@ -45,5 +74,7 @@ int main()
     h.insertAtHeap(20);
     h.insertAtHeap(30);
     h.insertAtHeap(5);
+    h.printHeadElement();
+    h.deleteHeapElement();
     h.printHeadElement();
 }
