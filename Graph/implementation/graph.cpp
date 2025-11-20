bb3b30dee5843d2d319c534541fcd4b9e68ef4fe
SoumenSen31 <soumensen411@gmail.com>
2025-11-20 13:11:51 +0600

    implement bfs

--- Graph/implementation/graph.cpp
+++ Graph/implementation/graph.cpp
@@ -9,49 +9,74 @@ public:
     Graph(int vertices)
     {
         this->newVertices = vertices;
-        adjList = new list<int>[vertices];
+        adjList = new list<int>[vertices+1];
     }
     void addEdges(int u, int v)
     {
-        if(u == v){
-        adjList[u].push_back(v);
-
+        if (u == v)
+        {
+            adjList[u].push_back(v);
         }
-        else{
-        adjList[u].push_back(v);
-        adjList[v].push_back(u);
+        else
+        {
+            adjList[u].push_back(v);
+            adjList[v].push_back(u);
         }
     }
 
     // bfs traversal
-    void bfs(){
-        queue<int>q;
-        q.push(0);
+    void bfs()
+    {
+        queue<int> q;
+        q.push(1);
+        vector<bool> visited(newVertices, false);
+        visited[1] = true;
+        while (q.size() > 0)
+        {
+            int u = q.front();
+            q.pop();
+            cout << u << " ";
+            for (auto v : adjList[u])
+            {
+                if (!visited[v])
+                {
+                    visited[v] = true;
+                    q.push(v);
+                }
+            }
+        }
     }
-
 
     void printList()
     {
-        for (int i = 0; i < newVertices; i++)
+        for (int i = 1; i <= newVertices; i++)
         {
             cout << i << " : ";
-            for (int height : adjList[i]){
-                cout << height<<" ";
+            for (int height : adjList[i])
+            {
+                cout << height << " ";
+            }
+            cout << endl;
         }
-        cout << endl;
     }
-}
 };
-int main()
+
+void graphOperations()
 {
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
     int numVertices;
-    cin>>numVertices;
+    cin >> numVertices;
     Graph g(numVertices);
-    int from ,to;
-    while(cin>>from>>to){
-    g.addEdges(from,to);
+    int from, to;
+    while (cin >> from >> to)
+    {
+        g.addEdges(from, to);
     }
     g.printList();
+    g.bfs();
+}
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    graphOperations();
 }
