c369613e5e5352732f68539c325cd9c5dec0388d
SoumenSen31 <soumensen411@gmail.com>
2025-11-20 10:24:29 +0600

    modify bst functions

--- Tree/binary_search_tree/binarySearchTree.cpp
+++ Tree/binary_search_tree/binarySearchTree.cpp
@@ -13,6 +13,7 @@ public:
         this->right = nullptr;
     }
 };
+
 Node *buildBinarySerarchTree(Node *root, int value)
 {
     if (root == NULL)
@@ -49,6 +50,17 @@ bool search(Node *root, int key)
         return search(root->right, key);
     }
 }
+// ================ TRAVERSAL ================
+
+void preorderTraversal(Node *root)
+{
+    if (root == NULL)
+        return;
+
+    cout << root->value << " ";
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+}
 
 void inorderTraversal(Node *root)
 {
@@ -58,16 +70,97 @@ void inorderTraversal(Node *root)
     cout << root->value << " ";
     inorderTraversal(root->right);
 }
-int main()
+
+void postorderTraversal(Node *root)
 {
-    freopen("output.txt","w",stdout);
+    if (root == NULL)
+        return;
+    postorderTraversal(root->left);
+    postorderTraversal(root->right);
+    cout << root->value << " ";
+}
+
+// ================ Delete Node form the bst ================
+Node *getInorderSuccessor(Node *root)
+{
+    while (root != NULL && root->left != NULL)
+    {
+        root = root->left;
+    }
+    return root;
+}
+
+Node *deleteNode(Node *root, int key)
+{
+    if (root == nullptr)
+    {
+        return NULL;
+    }
+    if (root->value > key)
+    {
+        root->left = deleteNode(root->left, key);
+    }
+    else if (root->value < key)
+    {
+        root->right = deleteNode(root->right, key);
+    }
+    else
+    {
+        if (root->left == nullptr)
+        {
+            Node *temp = root->right;
+            delete root;
+            return temp;
+        }
+        else if (root->right == nullptr)
+        {
+            Node *temp = root->left;
+            delete root;
+            return temp;
+        }
+        else
+        {
+            Node *inorderSuccessor = getInorderSuccessor(root->right);
+            root->value = inorderSuccessor->value;
+            root->right = deleteNode(root->right, inorderSuccessor->value);
+        }
+    }
+}
+
+void BSToperations()
+{
+
     vector<int> v = {50, 30, 20, 40, 35, 45, 70, 60, 65, 80, 90};
     Node *root = nullptr;
+    // insert data into bst
     for (auto i : v)
     {
         root = buildBinarySerarchTree(root, i);
     }
+    // traversal into bst
+    cout << "preorder BST Traversal: ";
+    preorderTraversal(root);
+    cout << endl;
+    cout << "Inorder BST Traversal: ";
     inorderTraversal(root);
-    cout<<endl;
-    cout << (search(root, 50) ? "Yes" : "NO") << endl; // searching value fram a bst 
+    cout << endl;
+    cout << "postorder BST Traversal: ";
+    postorderTraversal(root);
+    cout << endl;
+
+    // searching value fram a bst
+    cout << (search(root, 50) ? "Yes" : "NO") << endl; 
+    cout << (search(root, 100) ? "Yes" : "NO") << endl; 
+
+    // delete Node from bst
+    deleteNode(root, 35);
+    cout << "preorder BST Traversal: ";
+    preorderTraversal(root);
+    cout << endl;
+}
+
+int main()
+{
+    freopen("output.txt", "w", stdout);
+    BSToperations();
 }
