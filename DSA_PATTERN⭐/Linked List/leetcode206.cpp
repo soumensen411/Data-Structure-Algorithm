4488d79716b51785e338d4822ca7cf2e587eddd7
SoumenSen31 <soumensen411@gmail.com>
2026-02-25 21:16:09 +0600

    solve leetcode problem 206 with new pattern Linked list

---
+++ DSA_PATTERN⭐/Linked List/leetcode206.cpp
@@ -0,0 +1,65 @@
+// 206. Reverse Linked List
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *curr = head;
+        ListNode *prev = nullptr;
+        while (curr != nullptr)
+        {
+            ListNode *temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+};
+
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    cout << "Original list: ";
+    printList(head);
+
+    Solution s;
+    ListNode* reversedHead = s.reverseList(head);
+
+    cout << "Reversed list: ";
+    printList(reversedHead);
+
+    // Clean up memory
+    while (reversedHead != nullptr) {
+        ListNode* temp = reversedHead;
+        reversedHead = reversedHead->next;
+        delete temp;
+    }
+
+    return 0;
+}
