8dc18c1115ff26c13ffd5301d7c29b852326a1db
SoumenSen31 <soumensen411@gmail.com>
2026-03-08 09:49:29 +0600

    solve problem 153 with binary search pattern

---
+++ DSA_PATTERN⭐/Binary Search/tempCodeRunnerFile.cpp
@@ -0,0 +1,29 @@
+// 153. Find Minimum in Rotated Sorted Array
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    int findMin(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size() - 1;
+        while(high>low){
+            int mid = (low + high) / 2;
+            if(nums[mid]>nums[high]){
+                low = mid + 1;
+            }
+            else{
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+};
+
+int main(){
+    vector<int> v = {3,4,5,6,7,0,1,2};
+    vector<int> v = {11,12,13,15,17};
+    Solution s;
+    cout<<s.findMin(v);
+}
